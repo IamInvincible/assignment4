c7902ec0ead6b755833f7a8b262b68268d29b584
IamInvincible <jfang1498@utexas.edu>
2017-03-11 00:44:43 -0600

    Add files via upload

---
+++ src/assignment4/Critter2.java
@@ -0,0 +1,15 @@
+package assignment4;
+
+public class Critter2 extends Critter{
+	@Override
+	public void doTimeStep() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean fight(String oponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
