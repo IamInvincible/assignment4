fa2c4e31092f8b9600e20aacf555ffd6605f6381
IamInvincible <jfang1498@utexas.edu>
2017-03-11 17:41:31 -0600

    Add files via upload

--- src/assignment4/Critter2.java
+++ src/assignment4/Critter2.java
@@ -1,3 +1,4 @@
+/*Critter 2 runs in random directions and only walks if it has less than 50% of its starting energy (rounded down)*/
 package assignment4;
 
 public class Critter2 extends Critter{
@@ -8,7 +9,9 @@ public class Critter2 extends Critter{
 	@Override
 	public void doTimeStep() {
 		// TODO Auto-generated method stub
-		
+		if(this.getEnergy()<(Params.start_energy)/2){
+			this.run(this.getRandomInt(8));
+		}
 	}
 	@Override
 	public boolean fight(String oponent) {
