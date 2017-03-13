3a1e06e5a445e48617f289912e9826fde497b095
IamInvincible <jfang1498@utexas.edu>
2017-03-12 20:11:54 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 package assignment4;
-/* CRITTERS Main.java
+/* CRITTERS Critter.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
- * Slip days used: <0>
+ * Jason Fang
+ * jhf649
+ * 16238
+ * Cejay Zhu
+ * cz4723
+ * 16238
+ * Slip days used: 1
  * Fall 2016
  */
 
